c81057f5e7d8e499c145cedeb9a72ccf12ecf307
pjskeptic <psparrow25@gmail.com>
2015-07-15 14:45:11 -0400

    Refactor RequiresDisclaimer module

--- app/models/requires_disclaimer.rb
+++ app/models/requires_disclaimer.rb
@@ -1,13 +1,12 @@
 module RequiresDisclaimer
-  attr_accessor :disclaimer
-
   def disclaimer
-    ["1", true].include? @disclaimer
+    @disclaimer == '1'
   end
 
   def self.extended(base)
     base.instance_eval do
       class << self
+        attr_writer :disclaimer
         validates_presence_of :disclaimer
       end
     end
